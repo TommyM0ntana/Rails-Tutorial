4ee282f474511f89875b839fb9131fba197e3637
Tommy M0ntana <tommy.montana.harwood@gmail.com>
2020-01-07 20:18:54 +0100

    finish c5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,26 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-  get 'foo/baz'
-  get 'static_pages/home'
-  get 'static_pages/help'
+  # get 'foo/bar'
+  # get 'foo/baz'
   root 'static_pages#home'
-  get  'static_pages/about'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+
+  # get   '/help',    to:  'static_pages#help'
+  # get   '/about',   to:  'static_pages#about'
+  # get   '/contact', to:  'static_pages#contact'
+
+  #  root 'static_pages#home'
+ 
+
+  #  get 'static_pages/home'
+  #  get 'static_pages/help'
+  # # get '/help', to: 'static_pages#help'
+
+  #  get  'static_pages/about'
+  # # get '/about', to: 'static_pages#about'
+
+  # get  'static_pages/contact'
+  # # get '/contact', to: 'static_pages#contact'
 
 end
